d140d18df618bd6cb31e042840e0ae5fdb7218d9
munkh-altai <munkaltai@gmail.com>
2023-04-07 08:51:13 +0800

    vat and city tax calculator added

--- bill/helper.go
+++ bill/helper.go
@@ -13,3 +13,19 @@ func GenerateBillIdSuffix() string {
 func FormatNumber(n float64) string {
 	return fmt.Sprintf("%.2f", n)
 }
+
+func GetVat(amount float64, withCityTax bool) string {
+	if withCityTax {
+		return FormatNumber((amount / 111) * 10)
+	} else {
+		return FormatNumber((amount / 110) * 10)
+	}
+}
+
+func GetWithCityTax(amount float64, witVat bool) string {
+	if witVat {
+		return FormatNumber((amount / 111) * 1)
+	} else {
+		return FormatNumber((amount / 101) * 1)
+	}
+}
