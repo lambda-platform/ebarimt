e7838b46e25511195a59399fa5a4c6644668f9d5
munkh-altai <munkaltai@gmail.com>
2023-05-08 21:56:55 +0800

    create bill back

---
+++ bill/createBill.go
@@ -0,0 +1,40 @@
+package bill
+
+import (
+	"errors"
+	"github.com/lambda-platform/ebarimt/posapi"
+)
+
+func PutBill(input posapi.PutInput, api posapi.PosAPI) (posapi.PutOutput, error) {
+	// Check if the API is working properly.
+	checkRes, err := api.CheckApi()
+	if err != nil {
+		return posapi.PutOutput{}, err
+	}
+	if !checkRes.Config.Success || !checkRes.Database.Success || !checkRes.Network.Success || !checkRes.Success {
+		// If the API is not working properly, send any unsent data.
+		sendRes, err := api.SendData()
+		if err != nil {
+			return posapi.PutOutput{}, err
+		}
+		if !sendRes.Success {
+			return posapi.PutOutput{}, errors.New("failed to send data to server")
+		}
+		// Check the API again to ensure it is working properly.
+		checkRes2, err := api.CheckApi()
+		if err != nil {
+			return posapi.PutOutput{}, err
+		}
+		if !checkRes2.Config.Success || !checkRes2.Database.Success || !checkRes2.Network.Success || !checkRes2.Success {
+			return posapi.PutOutput{}, errors.New("EBarimt service error: please contact administrator")
+		}
+	}
+
+	// Call the "put" function in the shared library.
+	response, err := api.Put(input)
+	if err != nil {
+		return posapi.PutOutput{}, err
+	}
+
+	return response, nil
+}
